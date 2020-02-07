e3e41b6b9b19a9a657b98d5b304d9e610e0c114e
Kaj Neupert <kaj@neupert.dk>
2020-02-07 09:08:38 +0100

    * .gitignore: Initial setup
    * SmallBank.sln:
    * Program.cs:
    * SmallBank.csproj:

---
+++ SmallBank/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SmallBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
