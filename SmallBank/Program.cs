52810905e45c1fdb019fb55901625b9831f72ae4
Kaj Neupert <kaj@neupert.dk>
2020-02-13 09:34:11 +0100

    Tidied up

--- SmallBank/Program.cs
+++ SmallBank/Program.cs
@@ -11,7 +11,7 @@ namespace SmallBank
         static List<Customer> customers = new List<Customer>();
         static void Main(string[] args)
         {
-            Init();
+            customers = Test.Init();
             SeeCustomers();
 
             string cMenu = string.Empty;
@@ -84,18 +84,6 @@ namespace SmallBank
             
         }
 
-        private static Account FindAccountById(int currentAccountID)
-        {
-            foreach( Account a in currentCustomer.Accounts)
-            {
-                if (a.AccountID == currentAccountID)
-                {
-                    return a;
-                }
-            }
-            return null;
-        }
-
         private static Account NewAccountDialog()
         {
             string name = string.Empty;
@@ -109,7 +97,23 @@ namespace SmallBank
             Console.WriteLine("Kontoens rentefod:");
             double.TryParse(Console.ReadLine(), out interest);
 
-            return CreateAccount(type, interest, name);
+            switch (typeID)
+            {
+                case 1:
+                    return new Loan
+                    {
+                        Name = name,
+                        Interest = interest
+                    };
+                case 2:
+                default:
+                    return new Deposit
+                    {
+                        Name = name,
+                        Interest = interest
+                    };
+
+            }
         }
 
         private static double CalculateCapital()
@@ -125,18 +129,6 @@ namespace SmallBank
             return output;
         }
 
-        private static void Init()
-        {
-            string[] names =
-            {
-                "Anders Andersen", "Bob Bobson", "Chris Christofferson", "Dick Dickson", "Erik Eriksen"
-            };
-            foreach (string n in names)
-            {
-                customers.Add(CreateCustomers(n));
-            }
-        }
-
         private static Customer FindCustomerById(int id)
         {
             foreach(Customer c in customers)
@@ -144,6 +136,18 @@ namespace SmallBank
                 if ( c.CustomerID == id)
                 {
                     return c;
+                }
+            }
+            return null;
+        }
+
+        private static Account FindAccountById(int currentAccountID)
+        {
+            foreach (Account a in currentCustomer.Accounts)
+            {
+                if (a.AccountID == currentAccountID)
+                {
+                    return a;
                 }
             }
             return null;
@@ -174,72 +178,6 @@ namespace SmallBank
                     Console.WriteLine($"   {account.AccountID}: {account.Name} interest: {account.Interest}" +
                         $" {account.Balance}");
                 }
-        }
-
-        static Customer CreateCustomers(string name)
-        {
-            Customer a = new Customer();
-            a.Name = name;
-            a.Email = CreatePhoneyEmail(a.Name);
-            a.Telephone = CreatePhoneyPhoneNumber();
-            a.Address = CreatePhoneyAddress(a.Name);
-            Loan l = (Loan) CreateAccount("loan", 5, "billån");
-            a.Accounts.Add(l);
-            Deposit d = (Deposit)CreateAccount("deposit", 2, "lønkonto");
-            a.Accounts.Add(d);
-            return a;
-        }
-
-        static Account CreateAccount( string type, double interest, string name)
-        {
-            switch (type) {
-                case "loan":
-                    Loan a = new Loan
-                    {
-                        Interest = interest,
-                        Name = name
-                    };
-                    return a;
-                case "deposit":
-                default:
-                    Deposit b = new Deposit
-                    {
-                        Interest = interest,
-                        Name = name
-                    };
-                    return b;
-            }
-        }
-
-        private static string CreatePhoneyEmail(string name)
-        {
-            string[] a = name.Split(" ");
-
-            return a[0].ToLower() + "@" + a[1].ToLower() + ".dk";
-        }
-        private static Address CreatePhoneyAddress(string name)
-        {
-            string[] a = name.Split(" ");
-            Random rnd = new Random();
-            Address b = new Address
-            {
-                StreetAddress = a[1] + "svej",
-                HouseNumber = rnd.Next(1, 100).ToString(),
-                Zip = "5000",
-                City = "Odense"
-            };
-            return b;
-        }
-
-        static string CreatePhoneyPhoneNumber()
-        {
-            Random rnd = new Random();
-            string output = string.Empty;
-            for (int i= 0; i <8; i++)
-            {
-                output += rnd.Next(0, 10);
-            }
-            return output;
         }
     }
 }
--- SmallBank/Test.cs
+++ SmallBank/Test.cs
@@ -1,16 +1,93 @@
 ﻿using System;
+using System.Collections.Generic;
+using SmallBank.Model;
+
 namespace SmallBank
 {
-    public class Test
+    public static class Test
     {
 
-        public static void Init()
+        public static List<Customer> Init()
         {
-
+            List<Customer> customers = new List<Customer>();
+            string[] names =
+            {
+                "Anders Andersen", "Bob Bobson", "Chris Christofferson", "Dick Dickson", "Erik Eriksen"
+            };
+            foreach (string n in names)
+            {
+                customers.Add(CreateCustomers(n));
+            }
+            return customers;
         }
 
-        public Test()
+
+        static Customer CreateCustomers(string name)
         {
+            Customer a = new Customer();
+            a.Name = name;
+            a.Email = CreatePhoneyEmail(a.Name);
+            a.Telephone = CreatePhoneyPhoneNumber();
+            a.Address = CreatePhoneyAddress(a.Name);
+            Loan l = (Loan)CreateAccount("loan", 5, "billån");
+            a.Accounts.Add(l);
+            Deposit d = (Deposit)CreateAccount("deposit", 2, "lønkonto");
+            a.Accounts.Add(d);
+            return a;
         }
+
+        static Account CreateAccount(string type, double interest, string name)
+        {
+            switch (type)
+            {
+                case "loan":
+                    Loan a = new Loan
+                    {
+                        Interest = interest,
+                        Name = name
+                    };
+                    return a;
+                case "deposit":
+                default:
+                    Deposit b = new Deposit
+                    {
+                        Interest = interest,
+                        Name = name
+                    };
+                    return b;
+            }
+        }
+
+        private static string CreatePhoneyEmail(string name)
+        {
+            string[] a = name.Split(" ");
+
+            return a[0].ToLower() + "@" + a[1].ToLower() + ".dk";
+        }
+        private static Address CreatePhoneyAddress(string name)
+        {
+            string[] a = name.Split(" ");
+            Random rnd = new Random();
+            Address b = new Address
+            {
+                StreetAddress = a[1] + "svej",
+                HouseNumber = rnd.Next(1, 100).ToString(),
+                Zip = "5000",
+                City = "Odense"
+            };
+            return b;
+        }
+
+        static string CreatePhoneyPhoneNumber()
+        {
+            Random rnd = new Random();
+            string output = string.Empty;
+            for (int i = 0; i < 8; i++)
+            {
+                output += rnd.Next(0, 10);
+            }
+            return output;
+        }
+
     }
 }
